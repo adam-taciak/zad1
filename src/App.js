3bc759b2408ed5d038f5171cb106641c87e42242
Taciak <adam.taciak@fujitsu.com>
2022-10-13 09:48:10 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import Counter from './Counter'
+
+function App() {
+  return (
+    <div>
+      {/* <Counter /> */}
+    </div>
+  );
+}
+
+export default App;
+
+// Gdy komponent <Counter /> będzie gotowy,
+// usuń komentarze z linii 1 i 6
